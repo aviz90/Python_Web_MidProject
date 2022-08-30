b56c1b6e59d8cc74c1f5e39eae417de40f3e1798
azilbers <avi.zilbershtein@thalesgroup.com>
2022-08-30 08:13:49 +0300

    Mid_Project

---
+++ Classes.py
@@ -0,0 +1,163 @@
+from asyncio.windows_events import NULL
+from csv import reader
+from datetime import date
+from pymongo import MongoClient
+from bson import ObjectId
+
+client = MongoClient(port=27017)
+db = client["yanivDB"]
+books_collection = db["books"]
+
+class Book:
+    def __init__(self, author, title, nop):
+        self.author = author
+        self.title = title
+        self.num_of_pages = nop
+
+
+class Shelf:
+    def __init__(self):
+        self.books = []  # max 5 books
+        self.is_shelf_full = False
+
+    def addBook(self, book):
+        if len(self.books) < 5:
+            self.books.append(book)
+            print("New book has been added")
+        else:
+            print("No more spcae")
+            self.is_shelf_full = True
+
+    def replace_books(self, num1, num2):
+        try:
+            self.books[num1], self.books[num2 ] = self.books[num2], self.books[num1]
+            print("Location changed successfully")
+        except IndexError:       
+            print("Book location is empty")
+
+    def order_books(self):
+        # print(list((self.books[0].num_of_pages, self.books[1].num_of_pages)))
+
+        nopBooks = list(map(lambda book : book.num_of_pages ,self.books))
+        print(f"Before sorting {nopBooks}")
+
+        #sorting books list
+        booksSortedAsc = sorted(self.books, key=lambda book: book.num_of_pages)
+        print(f"After sorting {list(map(lambda book : book.num_of_pages ,booksSortedAsc))}")
+
+class Reader:
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+        self.books = []
+
+    def read_book(self, bookTitle):
+        today = date.today()
+        currentDate = today.strftime("%b-%d-%Y")
+        # print(currentDate)
+        self.books.append({"title": bookTitle, "date": currentDate})
+        print("book has been added to reader")        
+
+class Library:
+    def __init__(self, s1, s2, s3):
+        self.shelves = [s1, s2, s3] #list of 3 Shelf objects
+        self.readers = [] #list of Reader objects
+    
+    def is_there_place_for_new_book(self):
+        self.place_in_library == False
+        for shelf in self.shelves:
+            if shelf.is_shelf_full == False:
+                self.place_in_library == True
+                break
+        
+        return self.place_in_library
+
+    
+    #add book object to the first Shelf with a free space
+    def add_new_book(self, bookOBJ):
+        for shelf in self.shelves:
+            if shelf.is_shelf_full == False:
+                shelf.addBook(bookOBJ)
+                break
+
+    #delete the Book object from the library.
+    def delete_book(self, book_title):
+        for shelf in self.shelves:
+            for book in shelf.books:
+                if book.title == book_title:
+                    shelf.books.remove(book)
+                    print("Book has been deleted")
+                    # print(shelf.books[0].title)
+
+    #replace between 2 Books objects (their locations in the shelves).
+    def change_locations(self, bookTitle1, bookTitle2):
+        flag = True
+        for i in range(2):
+            if flag == False:
+                if (index_book1 and index_book2) is not None:
+                    self.change_locations_in_same_shelf(index_shelf, index_book1, index_book2)
+                else:
+                    print("Please replace books title in the same shelf")
+                return
+            for book in self.shelves[i].books:
+                if book.title == bookTitle1:
+                    index_book1 = self.shelves[i].books.index(book)
+                    index_shelf = i
+                    flag = False
+                if book.title == bookTitle2:
+                    index_book2 = self.shelves[i].books.index(book)
+                    index_shelf = i
+                    flag = False
+
+        # placeBook1 = self.shelves[index_shelf].books[index_book1]
+        # placeBook2 = self.shelves[index_shelf].books[index_book2]   
+        # placeBook1, placeBook2 = placeBook2, placeBook1
+        # print("Location changed successfully")        
+
+
+    #receives a shelf number, and books locations and replace between these 2 Books objects.
+    def change_locations_in_same_shelf(self, shelfNumber, bookLocation1, bookLocation2):
+        self.shelves[shelfNumber].replace_books(bookLocation1, bookLocation2)
+        # arrBooks[bookLocation1], arrBooks[bookLocation2] == arrBooks[bookLocation2], arrBooks[bookLocation1]
+        
+    #order all books in each shelf by their num_of_pages
+    def order_books(self):
+        for shelf in self.shelves:
+            shelf.order_books()
+
+    #receives a new reader name and id and add it to the readers list.
+    def register_reader(self, reader_id, reader_name):
+        reader = Reader(reader_id, reader_name)
+        self.readers.append(reader)
+        print(f"Your reader id: {reader_id} has been added")
+
+    #receives a reader name and removes it from the readers list.
+    def remove_reader(self, reader_name):
+        for reader in self.readers:
+            if reader.name == reader_name:
+                self.readers.remove(reader)
+                print(f"Reader {reader_name} has been removed")
+
+    #receives a book title and a reader name and add this book title to the reader’s books list
+    def reader_read_book(self, book_title, reader_name):
+        for reader in self.readers:
+            if reader.name == reader_name:
+                reader.read_book(book_title)
+        
+    #receives an author name and returns all books titles this author wrote
+    def search_by_author(self, author_name):
+        author_books = []
+        for shelf in self.shelves:
+            book_by_author = list(filter(lambda book: book.author == author_name ,shelf.books))
+            if len(book_by_author) > 0:
+                author_books = [*book_by_author]
+            else:
+                return False
+        # print(author_books)
+        books_titles_by_author = list(map(lambda book: book.title, author_books))
+        return books_titles_by_author
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,138 @@
+from Classes import *
+import json
+import os
+import sys
+
+
+
+mongo_books = books_collection.find({},{'_id':0})
+count = books_collection.count_documents({})
+
+booksOBJ = []
+for i in range(count):
+    booksOBJ.append(Book(mongo_books[i]["author"], mongo_books[i]["title"], mongo_books[i]["num_of_pages"]))
+
+shelf1 = Shelf()
+shelf2 = Shelf()
+shelf3 = Shelf()
+shelf1.books.append(booksOBJ[0])
+shelf1.books.append(booksOBJ[1])
+# print(shelf1.books)
+shelf2.books.append(booksOBJ[2])
+shelf2.books.append(booksOBJ[3])
+shelf3.books.append(booksOBJ[4])
+shelf3.books.append(booksOBJ[5])
+# reader = Reader()
+library = Library(shelf1, shelf2, shelf3)
+# shelf1.replace_books(1, 4)
+# shelf1.order_books()
+
+id = 0
+choice = ""
+def save_library(file_name):
+    file_name = file_name
+    with open(os.path.join(sys.path[0], file_name + ".json"), "w") as f:
+            
+        shelves = []
+        for shelf in library.shelves:
+            books = []
+            shelfObj = {}
+            shelfObj["is_shelf_full"] = shelf.is_shelf_full
+            for book in shelf.books:
+                bookObj = {}               
+                bookObj["author"] = book.author
+                bookObj["title"] = book.title
+                bookObj["num_of_pages"] = book.num_of_pages
+                books.append(bookObj)
+            
+            shelfObj["books"] = books
+            shelves.append(shelfObj)
+
+        readers = []
+        for r in library.readers:
+            books = []
+            readerObj = {}
+            readerObj["id"] = r.id
+            for book in r.books:
+                bookObj = {}               
+                bookObj["title"] = book["title"]
+                bookObj["date"] = book["date"]
+                books.append(bookObj)
+            
+            readerObj["books"] = books
+            readers.append(readerObj)
+
+        obj = {"shelves": shelves, "readers": readers}
+        json.dump(obj,f)
+
+while(choice != '11'):
+    choice = input("For adding a book - Press 1.\n"
+    "For deleting a book - Press 2.\n"
+    "For changing books locations - Press 3.\n"
+    "For registering a new reader - Press 4.\n"
+    "For removing a reader - Press 5.\n"
+    "For searching books by author – Press 6: \n"
+    "For reading a book by a reader – Press 7.\n"
+    "For ordering all books – Press 8.\n"
+    "For saving all data – Press 9.\n"
+    "For loading data – Press 10.\n"
+    "For exit – Press 11.\n")
+    
+    if choice == "1":
+        author = input("Insert author of book: ")
+        title = input("Insert title of book: ")
+        num_of_pages = input("Insert number of pages in book: ")
+        book = Book(author, title, num_of_pages)
+        library.add_new_book(book)
+        input("Back to Menu...")
+    elif choice == "2":
+        book_title = input("Type the title of book to be delete: ")
+        library.delete_book(book_title)
+        input("Back to Menu...")
+    elif choice == "3":
+        title1 = input("Type the title of first book to be replaced: ")
+        title2 = input("Type the title of second book to be replaced: ")
+        library.change_locations(title1, title2)
+        input("Back to Menu...")
+    elif choice == "4":
+        id += 1
+        reader_name = input("What is your name?: ")
+        library.register_reader(id, reader_name)
+        input("Back to Menu...")
+    elif choice == "5":
+        reader_name = input("Type reader name to be removed: ")
+        library.remove_reader(reader_name)
+        input("Back to Menu...")
+    elif choice == "6":
+        authour_name = input("Type the author name to search by: ")
+        books_titles_by_author = library.search_by_author(authour_name)
+        if books_titles_by_author:
+            print(books_titles_by_author)
+            for title in books_titles_by_author:
+                print(f'\"{title}\"')
+        else:
+            print("Author not found")
+        input("Back to Menu...")
+    elif choice == "7":
+        reader_id = input("Type the reader id: ")
+        try:
+            reader_name = list(filter(lambda reader: str(reader.id) == reader_id ,library.readers))[0].name
+        except IndexError:
+            print("Reader id not found.")
+        # reader_name = input("Type the reader name")
+        book_title = input("Type the title of book to read: ")
+        library.reader_read_book(book_title, reader_name)
+        input("Back to Menu...")
+    elif choice == "8":
+        library.order_books()
+        input("Back to Menu...")
+    elif choice == "9":
+        file_name = input("Type a file name to save the library data: ")
+        save_library(file_name)
+        print(f"Library saved in {file_name}")
+    elif choice == "10":
+        file_name = input("Type a file name to load a library data: ")
+        with open(os.path.join(sys.path[0], file_name + ".json"),'r') as f:
+            data = json.load(f)
+            library.shelves = data["shelves"]
+            library.readers = data["readers"]
